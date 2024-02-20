9af5dffc1c38336b0bf62efec96d886efb8eb33a
Rachel Peppe <rapeppe@hotmail.com>
2024-02-20 15:20:53 -0300

    criando o hero e importanto fonte.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass') (require ('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function imagens() {
+    return gulp.src('./src/imagens/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagens'));
+}
+
+exports.default = gulp.parallel(styles, imagens);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
